a6cbf6e4daa63f38abfb00ed3fb1a6a476875491
iamoric <pierratlucas@gmail.com>
2017-10-04 23:42:50 +0200

    argument with getopt

--- main.c
+++ main.c
@@ -4,20 +4,32 @@
     M1 CMI ISR
 **/
 
-
 //gcc -o main main.c -lpcap
 
 #include <stdio.h>
 #include <pcap.h>
+#include <getopt.h>
 #include "ethernet.h"
 
+//verbosity set to 1 by default
+int verbosity = 1;
 
+/**
+*/
 void got_packet(u_char* args, const struct pcap_pkthdr* header, const u_char* packet){
     handle_ethernet(packet);
 }
 
+/**
+*/
+void print_help(){
+    fprintf(stdout, "How to use :\n");
+    fprintf(stdout, "[-i interface]|[-o capture_file] [-f filter] [-v verbosity]");
+    fprintf(stdout, "verbosity between 1 (low) and 3 (high)\n");
+}
 
-int main() {
+
+int main(int argc, char **argv) {
 
     /**
     TODO :
@@ -32,7 +44,63 @@ int main() {
     char* errbuf;
     char* fileName = "traffic";
 
-    pcap_t* capture;
+    int option;
+    int interface = 0;
+
+    while((option = getopt(argc, argv, "hi:o:f:v:")) != -1) {
+        switch (option) {
+            //help
+            case 'h':
+                print_help();
+                return 0;
+            break;
+
+            //live interface
+            case 'i':
+                if (interface != 0) {
+                    fprintf(stdout, "You must choose between live and offline capture  !\n");
+                    return 0;
+                }
+                interface = 1;
+                //TODO gérer interface
+                printf("live\n");
+            break;
+
+            //offline interface
+            case 'o':
+                if (interface != 0) {
+                    fprintf(stdout, "You must choose between live and offline capture  !\n");
+                    return 0;
+                }
+                interface = 1;
+                //TODO gérer offline capture
+                printf("offline\n");
+            break;
+
+            //filter
+            case 'f':
+                //TODO gérer filtre
+                printf("filtre\n");
+            break;
+
+            //verbosity
+            case 'v':
+                //TODO gérer verbosity
+                printf("verbosity\n" );
+            break;
+
+            default:
+                print_help();
+                return 0;
+            break;
+        }
+    }
+
+    if (interface == 0){
+        print_help();
+    }
+
+    /*pcap_t* capture;
     capture = pcap_open_offline(fileName, errbuf);
 
     if (capture != 0) {
@@ -46,7 +114,7 @@ int main() {
     if (ret != 0) {
         //ERREUR
         printf("Erreur pcap_loop\n");
-    }
+    }*/
 
 
   return 0;
